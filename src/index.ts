0e3d8ae97d4020d1f814adbceba7fa5f3603f0dc
V Vetsin <vilinamogili423@gmail.com>
2025-05-11 20:11:08 +0200

    Update logging for meeting webhook event to include full request body

--- src/index.ts
+++ src/index.ts
@@ -223,7 +223,7 @@ app.post("/sendMessage", apiKeyAuth, async (req: any, res: any) => {
 
 app.post("/meetingWebhook", async (req: any, res: any) => {
   try {
-    logger.info("Received meeting webhook event", { event: req.body.event });
+    logger.info("Received meeting webhook event", { event: req.body });
 
     if (!req.body || !req.body.event) {
       return res
